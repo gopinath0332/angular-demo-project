783179999e3abc135a87b291652f2d1d2e0ff166
gopinath0332 <gopinath0332@outlook.com>
2020-12-10 05:31:48 -0800

    Initial commit

---
+++ src/app/users-component/users-component.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-users-component",
+  templateUrl: "./users-component.component.html",
+  styleUrls: ["./users-component.component.css"]
+})
+export class UsersComponentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
